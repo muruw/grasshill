677dfeb917dfaf94080574c65ec09b4ae22430be
muruw <murumaem@gmail.com>
2020-01-23 05:38:11 +0200

    Initialized new SPRING BOOT application

---
+++ src/main/java/com/muruw/grasshill/GrasshillApplication.java
@@ -0,0 +1,13 @@
+package com.muruw.grasshill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrasshillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrasshillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muruw/grasshill/GrasshillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muruw.grasshill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrasshillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
